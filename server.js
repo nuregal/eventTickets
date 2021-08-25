a2f0a0485ef887c4b78ab28a9c9f385063323f5c
Nur Egal <nuregal87@gmail.com>
2021-08-25 09:57:37 +0200

    Port update

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 app.use(router)
 
+const PORT = process.env.PORT || 3000  
 
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server started on port 3000");
 });
